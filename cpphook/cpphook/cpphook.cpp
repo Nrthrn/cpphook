d6663234c235cb4a57e853a1de826d20005ea01c
Kello <155478453+Nrthrn@users.noreply.github.com>
2024-09-27 21:10:23 +0300

    I HAVE SOMETHINGG </3

--- cpphook/cpphook/cpphook.cpp
+++ cpphook/cpphook/cpphook.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
 #include <windows.h>
+#include <iomanip>
+#include <sstream>
 
 namespace var {
     int input = 0;
 }
 
+void* read_mem() {
+    std::string input;
+    std::cout << "Enter the memory address (in hex, e.g., 0x12345678): ";
+    std::cin >> input;
+
+    uintptr_t address;
+    std::stringstream ss;
+    ss << std::hex << input;
+    ss >> address;
+
+    void* ptr = reinterpret_cast<void*>(address);
+    return ptr;
+}
+
+
 void menu()
 {
-    std::cout << "              [1] Test\n\nInput: ";
+    std::cout << "              [1] Read Mem\n\nInput: ";
 
     std::cin >> var::input;
 
     switch (var::input)
     {
     case 1:
-        std::cout << "Hello, World!\n";
-        Sleep(1000);
+        void* mem_ptr = read_mem();
+
+        int value = *reinterpret_cast<int*>(mem_ptr);
+        std::cout << "Value at address: " << std::hex << mem_ptr << " is " << value << std::endl;
+        Sleep(2000);
         system("cls");
     }
 }
 
 int main()
 {
-    std::cout << "Welcome to CPPHook!\n";
-    Sleep(1000);
-    std::cout << "Loading.\n";
-    Sleep(400);
-    std::cout << "Loading..\n";
-    Sleep(400);
-    std::cout << "Loading...\n";
-    Sleep(400);
-    system("cls");
-    std::cout << "Done!";
-    Sleep(100);
-    system("cls");
-
     while (true)
     {
         menu();
